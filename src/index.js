3e1825d6e2b85e9d73e449d617f905ec6467637f
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-03-29 14:23:44 +0200

    added the forecast

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+
   let day = days[date.getDay()];
   return `${day} ${formatHours(timestamp)}`;
 }
@@ -40,7 +41,6 @@ function displayTemperature(response) {
   celsiusTemperature = response.data.main.temp;
 
   temperatureElemement.innerHTML = Math.round(celsiusTemperature);
-  temperatureElemement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -54,11 +54,36 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class="col-2">
+  <h3>${formatHours(forecast.dt * 1000)}</h3>
+  <img src= "http://openweathermap.org/img/wn/${
+    forecast.weather[0].icon
+  }@2x.png"/>
+  <div class="weather-forecast-temperature">
+  <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(
+      forecast.main.temp_min
+    )}°
+  </div>
+  </div> `;
+  }
+}
+
 function search(city) {
   let apiKey = "97a509323a282fbb09c0bc8556148a31";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {

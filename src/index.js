19513b07124df09714baaad433d5abb7274813c4
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-03-30 17:19:05 +0200

    changed last updated time to local time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
+function formatDate(date, timezone) {
+  let localOffsetInMs = date.getTimezoneOffset() * 60 * 1000;
+  let targetOffsetInMs = timezone * 1000;
+  let targetTimestamp = date.getTime() + localOffsetInMs + targetOffsetInMs;
 
+  let localDate = new Date(targetTimestamp);
+
+  let dayIndex = localDate.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -10,9 +15,70 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+  let day = days[dayIndex];
 
-  let day = days[date.getDay()];
-  return `${day} ${formatHours(timestamp)}`;
+  let monthIndex = localDate.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[monthIndex];
+  let dateIndex = localDate.getDate();
+  let year = localDate.getFullYear();
+
+  let hours = localDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = localDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let currentTime = `${hours}:${minutes}`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = `${day} ${currentTime}, ${month} ${dateIndex}, ${year}`;
+
+  let formattedDate = `${day} ${currentTime}, <br/>
+  ${month} ${dateIndex}, ${year}`;
+
+  return formattedDate;
+}
+
+function displayTemperature(response) {
+  let temperatureElemement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#temperature-description");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let pressureElement = document.querySelector("#pressure");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElemement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  pressureElement.innerHTML = response.data.main.pressure;
+  dateElement.innerHTML = formatDate(new Date(), response.data.timezone);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function formatHours(timestamp) {
@@ -28,32 +94,6 @@ function formatHours(timestamp) {
   return `${hours}:${minutes}`;
 }
 
-function displayTemperature(response) {
-  let temperatureElemement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#temperature-description");
-  let windElement = document.querySelector("#wind");
-  let humidityElement = document.querySelector("#humidity");
-  let pressureElement = document.querySelector("#pressure");
-  let timeElement = document.querySelector("#time");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature = response.data.main.temp;
-
-  temperatureElemement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  humidityElement.innerHTML = response.data.main.humidity;
-  pressureElement.innerHTML = response.data.main.pressure;
-  timeElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = null;
@@ -61,6 +101,7 @@ function displayForecast(response) {
 
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
+
     forecastElement.innerHTML += `
   <div class="col-2">
   <h3>${formatHours(forecast.dt * 1000)}</h3>
@@ -94,7 +135,7 @@ function handleSubmit(event) {
 
 function displayFahreheitTemperature(event) {
   event.preventDefault();
-  celsisuLink.classList.remove("active");
+  celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let temperatureElemement = document.querySelector("#temperature");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
@@ -103,7 +144,7 @@ function displayFahreheitTemperature(event) {
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsisuLink.classList.add("active");
+  celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   let temperatureElemement = document.querySelector("#temperature");
   temperatureElemement.innerHTML = Math.round(celsiusTemperature);
@@ -117,7 +158,7 @@ form.addEventListener("submit", handleSubmit);
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahreheitTemperature);
 
-let celsisuLink = document.querySelector("#celsius-link");
-celsisuLink.addEventListener("click", displayCelsiusTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
